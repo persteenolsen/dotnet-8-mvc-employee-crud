9a92e6b047bc70bfc45d939cdcd32941f095e407
Per Olsen <persteen22@gmail.com>
2025-03-28 07:50:15 +0100

    Adjusted the model validation + bootstrap scroll

--- MvcWebApp/Models/Employee.cs
+++ MvcWebApp/Models/Employee.cs
@@ -9,13 +9,13 @@ namespace MvcWebApp.Models
 
         [Required]
         [Display(Name ="Employee Name")]
-        [StringLength(25, MinimumLength = 2)]
+        [StringLength(15, MinimumLength = 2)]
         public string Name { get; set; }
         
-        [StringLength(25, MinimumLength = 5)]
+        [StringLength(15, MinimumLength = 5)]
         public string Designation { get; set; }
 
-        [StringLength(50, MinimumLength = 5)]
+        [StringLength(20, MinimumLength = 5)]
         [DataType(DataType.MultilineText)]
         public string Address { get; set; } 
 
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ ASP.NET Core MVC Web Application by .NET 8 serving CRUD of Employees towards a M
 
 Last updated:
 
-- 13-03-2025
+- 28-03-2025
 
 # Create a global json
 
